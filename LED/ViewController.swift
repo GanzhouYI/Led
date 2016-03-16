32af5e2dc9cf1563359e89fd6dc3b0c1f2c0211c
123 <a123@lab205bdeimac-24.local>
2016-03-16 11:49:56 +0800

    完成

--- LED/ViewController.swift
+++ LED/ViewController.swift
@@ -10,8 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LightSource: UIView!
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    
+    
+    @IBOutlet weak var con: UISwitch!
+    
+    
+    @IBAction func setLightSourceAlphaValue(sender: UISlider) {
+        
+        let userDefault = NSUserDefaults.standardUserDefaults()//返回NSUserDefaults对象
+        userDefault.setBool(self.con.on, forKey: "onOff")
+        userDefault.setFloat(self.slider.value, forKey: "brightness")
+        userDefault.synchronize()//同步
+        
+        if self.con.on{
+            self.LightSource.alpha = CGFloat(self.slider.value)
+        }else{
+            self.LightSource.alpha = 0.0
+        }
+        
+        
+        
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //首选项
+        let userDefault = NSUserDefaults.standardUserDefaults()//返回NSUserDefaults对象
+        self.slider.value = userDefault.floatForKey("brightness")
+        self.con.on = userDefault.boolForKey("onOff")
+        
+        if self.con.on{
+            self.LightSource.alpha = CGFloat(self.slider.value)
+        }else{
+            self.LightSource.alpha = 0.0
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
